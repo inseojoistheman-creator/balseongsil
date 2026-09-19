58aedefa7573aa5fb6bea028ff8714a8729b08af
inseojoistheman-creator <inseojoistheman@gmail.com>
2026-09-19 14:51:46 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='bal-v2';
+const CACHE='bal-v3';
 const CORE=["./index.html", "./manifest.json", "./icon-180.png", "./icon-512.png"];
 self.addEventListener('install',e=>{ e.waitUntil(caches.open(CACHE).then(async c=>{ for(const u of CORE){ try{ await c.add(u); }catch(err){} } self.skipWaiting(); })); });
 self.addEventListener('activate',e=>{ e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())); });
